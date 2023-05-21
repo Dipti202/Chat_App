2964f8cdbcbc795fb018e67cefdfcfc4e6e62fd1
Dipti202 <dipti.kushwaha1988@gmail.com>
2023-05-21 18:11:31 +0530

    initalizes firebase project

---
+++ chat_wep_app/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import 'rsuite/dist/styles/rsuite-default.css';
+import './styles/main.scss'
+function App() {
+  return (
+    <div className="App">
+      hello
+    </div>
+  );
+} 
+
+export default App;
---
+++ chat_wep_app/src/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+const config = {
+  apiKey: "AIzaSyCwU4954ZzMQSRcqqycO-ST82Se_RraZJA",
+  authDomain: "chat-1406d.firebaseapp.com",
+  projectId: "chat-1406d",
+  storageBucket: "chat-1406d.appspot.com",
+  messagingSenderId: "195288185150",
+  appId: "1:195288185150:web:4e9e4c910c7a7e2f7fd46f"
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(config);
